f32a13ce911fb73cb65825486854cc6de7d92ac0
Ramz Ville <ramzville22@gmail.com>
2024-01-14 03:08:49 +0800

    Commit 2nd

--- src/js/main-card-modal.js
+++ src/js/main-card-modal.js
@@ -2,14 +2,17 @@ import { fetchInfoEvent, chooseBestImage } from './fetch-data';
 import MicroModal from 'micromodal';
 import renderCards from './main-cards';
 const modalContainer = document.querySelector('.main-card-modal-container');
-const modalContentElem = document.querySelector('.modal__content');
-const modalTopImage = document.querySelector('.main-card-modal-top_image');
+const modalContentElem = document.querySelector('.main-card-modal-content');
+const modalTopImage = document.querySelector('.main-card-modal-top-image');
 const eventImageElem = document.querySelector('.event__image');
 const infoElem = document.querySelector('.event__info');
-const infoTitleElem = document.querySelector('.event__info_tilte');
+const infoTitleElem = document.querySelector('.event__info_title');
 const startDateElem = document.querySelector('.event__start_date');
+const placeElemTab = document.querySelector('.event__place-tab');
 const placeElem = document.querySelector('.event__place');
 const whoElem = document.querySelector('.event__who');
+const pricesElemMob = document.querySelector('.event__prices-mob');
+const whoElemMob = document.querySelector('.event__who-mob');
 const pricesElem = document.querySelector('.event__prices');
 const cardsElem = document.querySelector('.main-cards');
 const moreAuthorBtn = document.querySelector('.event__btn2');
@@ -31,7 +34,9 @@ export async function updateModalData(eventId) {
   modalTopImage.innerHTML = '';
   eventImageElem.innerHTML = '';
   pricesElem.innerHTML = '';
+  pricesElemMob.innerHTML = '';
   placeElem.innerHTML = '';
+  placeElemTab.innerHTML = '';
 
   modalTopImage.insertAdjacentHTML(
     'beforeend',
@@ -45,8 +50,9 @@ export async function updateModalData(eventId) {
 
   infoElem.textContent = eventData.info || eventData.description;
   if (!infoElem.textContent) {
-    infoElem.style = 'display: none';
-    infoTitleElem.style = 'display: none';
+    infoElem.style = 'display: block';
+    infoTitleElem.style = 'display: block';
+    infoElem.innerHTML = 'There is no available information for this event.';
   } else {
     infoElem.style = 'display: block';
     infoTitleElem.style = 'display: block';
@@ -61,9 +67,19 @@ export async function updateModalData(eventId) {
 
 
   whoElem.textContent = eventData.name;
+  whoElemMob.textContent = eventData.name;
 
   for (const venue of eventData._embedded.venues) {
     placeElem.insertAdjacentHTML(
+      'beforeend',
+      `
+    ${venue.city.name}, ${venue.country.name}
+    <br>
+    ${venue.name}
+    `
+    );
+
+    placeElemTab.insertAdjacentHTML(
       'beforeend',
       `
     ${venue.city.name}, ${venue.country.name}
@@ -77,9 +93,22 @@ export async function updateModalData(eventId) {
   if (!eventData.priceRanges) {
 
     pricesElem.innerHTML = 'There are no tickets available for this event';
+    pricesElemMob.innerHTML = 'There are no tickets available for this event';
   } else {
     for (const priceRange of eventData.priceRanges) {
       pricesElem.insertAdjacentHTML(
+        'beforeend', `
+          <div class="event__single_price">
+          <div>
+              ${priceRange.type.toUpperCase()} ${priceRange.min}-${priceRange.max} ${priceRange.currency}
+            </div>
+            <a class="event__btn1 event__buy_tickets" href="${
+              eventData.url
+            }" target="_blank">BUY TICKETS</a>
+          </div>
+      `);
+
+      pricesElemMob.insertAdjacentHTML(
         'beforeend', `
           <div class="event__single_price">
           <div>
